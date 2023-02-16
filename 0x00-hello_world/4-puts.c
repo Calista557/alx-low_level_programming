0f3003fda3f1c9c0719f8af564b57c912428039c
Calista557 <Nwannecalista101@gmail.com>
2023-02-16 02:48:24 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
-/**
- * main -is the entry point for the program.
- * 
- *  Return:zero if no error, Non-zero value if errors
- */
-int main(void)
-{ 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
-}
+/** 
+  * main -is the entry point for the program. 
+  * 
+  * Return:zero if no error, Non-zero value  if errors 
+  */ 
+ int main(void) 
+ { 
+         puts("\"Programming is like building a multilingual puzzle"); 
+         return (0); 
+ }
