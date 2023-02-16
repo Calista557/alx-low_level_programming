d0f40d3a6d987668abde32f8b372f09f9f7e2e45
Calista557 <Nwannecalista101@gmail.com>
2023-02-16 02:19:20 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,6 @@
  *  Return:zero if no error, Non-zero value if errors
  */
 int main(void)
-{ 	puts("\"Programming is like building a multilingual puzzle"
-);
+{ 	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
