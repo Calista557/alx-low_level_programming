d9ea834f05b0c40d012718205ff89775b1e21844
Calista557 <Nwannecalista101@gmail.com>
2023-02-17 08:48:46 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Decription: prints the lower case of the alphabet
+ *
+ * Return: 0, if succesful
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a' ; lc <= 'z' ; lc++)
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}
