9bd9bad6e1465392d7484188ab9438042ce84f2d
Calista557 <Nwannecalista101@gmail.com>
2023-02-17 07:55:05 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
+/*more headers goes there */
 /**
  * main -Entry point
  *
- * Description: generates determines if a number is positive, negative oe zero.-
+ * Description: generates random numbers, declares + OR -
  *
  *  Return: Always (0) Success
 */
